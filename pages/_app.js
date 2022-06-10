b959552b72124e33fda3d76d262ad109b7e7bd4a
Cagan Sevencan <36283663+cagansevencan@users.noreply.github.com>
2022-06-09 20:01:46 -0700

    Firestore, Chakra UI, Absolute Imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,47 @@
-import { ProvideAuth } from '../lib/auth'
-import '../styles/globals.css'
+import { CSSReset, ThemeProvider } from '@chakra-ui/core';
+import { Global, css } from '@emotion/core';
+import { AuthProvider } from '@/lib/auth'
+import customTheme from '@/styles/theme';
 
-function MyApp({ Component, pageProps }) {
-  return <ProvideAuth>
-    <Component {...pageProps} />
-  </ProvideAuth>
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+        
+          ::selection{
+          background-color: #47a3f3
+          color: #fefefe;
+          }
+
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+       
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={customTheme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
+  )
 }
 
-export default MyApp
+export default App
