7db23f76f92ab6a31547f1390183b35ff517dc8b
Cagan Sevencan <36283663+cagansevencan@users.noreply.github.com>
2022-08-10 14:51:31 -0700

    Update [siteId].js

--- pages/p/[siteId].js
+++ pages/p/[siteId].js
@@ -17,7 +17,7 @@ export async function getStaticProps(context) {
         props: {
             initialFeedback: feedback
         },
-        unstable_revalidate: 1
+        revalidate: 1
     }
 }
 
